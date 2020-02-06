9ef1ec5114ff4bbcd8b5ce8bd491c882e07010fd
LuisGilGB <luis.gilgb@gmail.com>
2020-02-06 13:07:03 +0100

    Margins and axis location

--- demos/bars-chart/index.js
+++ demos/bars-chart/index.js
@@ -1,27 +1,35 @@
 const WIDTH = 600;
 const HEIGHT = 600;
 
+const SMALL_MARGIN = 20;
+const LARGE_MARGIN = 100;
+
+const margin = {top: SMALL_MARGIN, right: SMALL_MARGIN, bottom: LARGE_MARGIN, left: LARGE_MARGIN}
+const chartWidth = WIDTH - margin.left - margin.right;
+const chartHeight = HEIGHT - margin.top - margin.bottom;
+
 const svg = d3.select('.d3-canvas')
     .append('svg')
         .attr('width', WIDTH)
         .attr('height', HEIGHT);
 
 const graphGroup = svg.append('g')
-    .attr('width', WIDTH)
-    .attr('height', HEIGHT);
+    .attr('width', chartWidth)
+    .attr('height', chartHeight)
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 const xAxisGroup = graphGroup.append('g')
-    .attr('transform', `translate(0, ${WIDTH})`);
+    .attr('transform', `translate(0, ${chartHeight})`);
 
 const yAxisGroup = graphGroup.append('g');
 
 const x = d3.scaleBand()
-    .range([0, WIDTH])
+    .range([0, chartWidth])
     .paddingInner(0.2)
     .paddingOuter(0.2);
 
 const y = d3.scaleLinear()
-    .range([HEIGHT, 0]);
+    .range([chartHeight, 0]);
 
 const xAxis = d3.axisBottom(x);
 const yAxis = d3.axisLeft(y)
