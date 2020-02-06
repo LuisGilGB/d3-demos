74372f3100912c25ad2878ec31a2d40eaaa82f5b
LuisGilGB <luis.gilgb@gmail.com>
2020-02-06 14:27:18 +0100

    Axis fixes and import of json data

--- demos/bars-chart/index.js
+++ demos/bars-chart/index.js
@@ -1,40 +1,43 @@
-const WIDTH = 600;
-const HEIGHT = 600;
+d3.json('data.json').then(data => {
+    console.log(data)
+    const WIDTH = 600;
+    const HEIGHT = 600;
 
-const SMALL_MARGIN = 20;
-const LARGE_MARGIN = 100;
+    const SMALL_MARGIN = 20;
+    const LARGE_MARGIN = 100;
 
-const margin = {top: SMALL_MARGIN, right: SMALL_MARGIN, bottom: LARGE_MARGIN, left: LARGE_MARGIN}
-const chartWidth = WIDTH - margin.left - margin.right;
-const chartHeight = HEIGHT - margin.top - margin.bottom;
+    const margin = {top: SMALL_MARGIN, right: SMALL_MARGIN, bottom: LARGE_MARGIN, left: LARGE_MARGIN}
+    const chartWidth = WIDTH - margin.left - margin.right;
+    const chartHeight = HEIGHT - margin.top - margin.bottom;
 
-const svg = d3.select('.d3-canvas')
-    .append('svg')
-        .attr('width', WIDTH)
-        .attr('height', HEIGHT);
+    const svg = d3.select('.d3-canvas')
+        .append('svg')
+            .attr('width', WIDTH)
+            .attr('height', HEIGHT);
 
-const graphGroup = svg.append('g')
-    .attr('width', chartWidth)
-    .attr('height', chartHeight)
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+    const graphGroup = svg.append('g')
+        .attr('width', chartWidth)
+        .attr('height', chartHeight)
+        .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-const xAxisGroup = graphGroup.append('g')
-    .attr('transform', `translate(0, ${chartHeight})`);
+    const xAxisGroup = graphGroup.append('g')
+        .attr('transform', `translate(0, ${chartHeight})`);
 
-const yAxisGroup = graphGroup.append('g');
+    const yAxisGroup = graphGroup.append('g');
 
-const x = d3.scaleBand()
-    .range([0, chartWidth])
-    .paddingInner(0.2)
-    .paddingOuter(0.2);
+    const x = d3.scaleBand()
+        .range([0, chartWidth])
+        .paddingInner(0.2)
+        .paddingOuter(0.2);
 
-const y = d3.scaleLinear()
-    .range([chartHeight, 0]);
+    const y = d3.scaleLinear()
+        .range([chartHeight, 0]);
 
-const xAxis = d3.axisBottom(x);
-const yAxis = d3.axisLeft(y)
-    .ticks(3)
-    .tickFormat(d => `d + ${units}`);
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3.axisLeft(y)
+        .ticks(3)
+        .tickFormat(d => `${d} units`);
 
-xAxisGroup.call(xAxis);
-yAxisGroup.call(yAxis);
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
+});
